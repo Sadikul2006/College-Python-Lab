91e076373796074b82c36b99884dd63258ea27c0
Sadikul Seikh <sadikul@example.com>
2025-09-04 12:08:01 +0530

    push to git

---
+++ Q30.py
@@ -0,0 +1,12 @@
+# 30. Write a Python program to find sum of elements in list.
+
+n = int(input("Enter number of elements: "))
+list = []
+
+for i in range(n):
+    element = int(input(f"Enter element {i+1}: "))
+    list.append(element)
+
+total = sum(list)
+print("List:",list)
+print("Sum of elements in list:", total)
